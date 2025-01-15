530f86603dc9540a16b1150fab276dd7d3ff73eb
mikespub <github@mikespub.net>
2025-01-15 15:44:52 +0100

    remove dummy main

--- class/usergui.php
+++ class/usergui.php
@@ -29,17 +29,5 @@ sys::import('modules.images.class.userapi');
  */
 class UserGui extends UserGuiClass
 {
-    /**
-     * User main GUI function
-     * @param array<string, mixed> $args
-     * @return array<mixed>
-     */
-    public function main(array $args = [])
-    {
-        $args['description'] ??= 'Description of images';
-
-        // Pass along the context for xarTpl::module() if needed
-        $args['context'] ??= $this->getContext();
-        return $args;
-    }
+    // ...
 }
